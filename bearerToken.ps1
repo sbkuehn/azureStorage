71e1baf9a1f268e6000c3c0e0f94acf5c4d88cd6
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-08-24 11:41:09 -0500

    Create bearerToken.ps1

---
+++ bearerToken.ps1
@@ -0,0 +1,47 @@
+<#
+Created 
+2018.12.20 
+Shannon Kuehn
+Last Updated
+2019.08.16
+
+© 2018 Microsoft Corporation. 
+All rights reserved. Sample scripts/code provided herein are not supported under any Microsoft standard support program 
+or service. The sample scripts/code are provided AS IS without warranty of any kind. Microsoft disclaims all implied 
+warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. 
+The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event 
+shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for 
+any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of 
+business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or 
+documentation, even if Microsoft has been advised of the possibility of such damages.
+
+Adapted from: 
+https://www.codeisahighway.com/how-to-easily-and-silently-obtain-accesstoken-bearer-from-an-existing-azure-powershell-session/
+https://gallery.technet.microsoft.com/scriptcenter/Easily-obtain-AccessToken-3ba6e593
+#>
+
+
+function Get-AzCachedAccessToken()
+{
+    $ErrorActionPreference = 'Stop'
+  
+    if(-not (Get-Module Az.Accounts)) {
+        Import-Module Az.Accounts
+    }
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    if(-not $azProfile.Accounts.Count) {
+        Write-Error "Ensure you have logged in before calling this function."    
+    }
+  
+    $currentAzureContext = Get-AzContext
+    $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
+    Write-Debug ("Getting access token for tenant" + $currentAzureContext.Tenant.TenantId)
+    $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
+    $token.AccessToken
+}
+
+function Get-AzBearerToken()
+{
+    $ErrorActionPreference = 'Stop'
+    ('Bearer {0}' -f (Get-AzCachedAccessToken))
+}
