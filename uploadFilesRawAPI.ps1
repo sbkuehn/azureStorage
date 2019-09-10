102ee0d1346d10afb170c97e53a2ccad8fed4b2a
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-09-09 21:29:24 -0500

    Create uploadFilesRawAPI.ps1

---
+++ uploadFilesRawAPI.ps1
@@ -0,0 +1,53 @@
+<#
+© 2019 Microsoft Corporation. 
+All rights reserved. Sample scripts/code provided herein are not supported under any Microsoft standard support program 
+or service. The sample scripts/code are provided AS IS without warranty of any kind. Microsoft disclaims all implied 
+warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. 
+The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event 
+shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for 
+any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of 
+business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or 
+documentation, even if Microsoft has been advised of the possibility of such damages.
+
+.SYNOPSIS
+  Stores a number of variables, grabs all files sitting in a local or remote file share, and uploads the files to an Azure Storage Account.
+
+.DESCRIPTION
+  This script will upload files to Azure Storage with a PutBlob call, using a shared access signature key. 
+
+.PARAMETER 
+  Required: storage account name, storage account container, shared access signature key, file path, and files
+
+.INPUTS
+  None
+
+.OUTPUTS
+  None
+
+.NOTES
+  Version:        1.0
+  Author:         Shannon Kuehn
+  Creation Date:  2019.09.09
+  Purpose/Change: Initial script development
+#>
+
+
+param(
+    [Parameter(Mandatory=$true)]
+    [int]$storageAccountName,
+    [Parameter(Mandatory=$true)]
+    [int]$storageContainer,
+    [Parameter(Mandatory=$true)]
+    [string]$sasKey,
+    [Parameter(Mandatory=$true)]
+    [string]$filePath
+)
+
+$files = Get-ChildItem -Path "\\server\share\folder" -Recurse -Force
+$headers = @{}
+$headers.Add("x-ms-blob-type","BlockBlob")
+
+ForEach ($file in $files){
+    $uri = "https://"+$storageAccountName+".blob.core.windows.net/"+$storageContainer+"/"+$file+$sasKey
+    Invoke-RestMethod -uri $uri -Method Put -InFile $file -ContentType "plain/text" -Headers $headers -Verbose
+}
